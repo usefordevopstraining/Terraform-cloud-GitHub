a0ce5bd534ec88aaa5f5eb2560c5464bb28f84a6
usefordevopstraining <163764814+usefordevopstraining@users.noreply.github.com>
2024-05-14 13:04:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.8.3" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
